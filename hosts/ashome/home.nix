aef17f65bf7c131b89856b4ab134f03c79606aff
alexspringer <alexspringer@pm.me>
2025-02-10 09:54:52 -0600

    aliases

--- hosts/ashome/home.nix
+++ hosts/ashome/home.nix
@@ -6,6 +6,19 @@
 }:
 let
   inherit (import ./variables.nix) gitUsername gitEmail;
+  myAliases = {
+    sv = "sudo nvim";
+    fr = "nh os switch --hostname ${host} /home/${username}/zaneyos";
+    fu = "nh os switch --hostname ${host} --update /home/${username}/zaneyos";
+    zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
+    ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+    v = "nvim";
+    cat = "bat";
+    ls = "eza --icons";
+    ll = "eza -lh --icons --grid --group-directories-first";
+    la = "eza -lah --icons --grid --group-directories-first";
+    ".." = "cd ..";
+  };
 in
 {
   # Home Manager Settings
@@ -182,19 +195,22 @@ in
           source $HOME/.bashrc-personal
         fi
       '';
-      shellAliases = {
-        sv = "sudo nvim";
-        fr = "nh os switch --hostname ${host} /home/${username}/zaneyos";
-        fu = "nh os switch --hostname ${host} --update /home/${username}/zaneyos";
-        zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
-        ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
-        v = "nvim";
-        cat = "bat";
-        ls = "eza --icons";
-        ll = "eza -lh --icons --grid --group-directories-first";
-        la = "eza -lah --icons --grid --group-directories-first";
-        ".." = "cd ..";
-      };
+
+      shellAliases = myAliases;
+
+      # {
+      #   sv = "sudo nvim";
+      #   fr = "nh os switch --hostname ${host} /home/${username}/zaneyos";
+      #   fu = "nh os switch --hostname ${host} --update /home/${username}/zaneyos";
+      #   zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
+      #   ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+      #   v = "nvim";
+      #   cat = "bat";
+      #   ls = "eza --icons";
+      #   ll = "eza -lh --icons --grid --group-directories-first";
+      #   la = "eza -lah --icons --grid --group-directories-first";
+      #   ".." = "cd ..";
+      # };
     };
     home-manager.enable = true;
     hyprlock = {
