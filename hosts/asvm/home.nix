cf288351dfc9128379f38da8735a6e818f00c39e
Alex Springer <alexspringer@pm.me>
2025-02-13 10:26:59 +0000

    added vm host

---
+++ hosts/asvm/home.nix
@@ -0,0 +1,408 @@
+{
+  pkgs,
+  lib,
+  username,
+  host,
+  ...
+}:
+let
+  inherit (import ./variables.nix) gitUsername gitEmail;
+  myAliases = {
+    sv = "sudo nvim";
+    fr = "nh os switch --hostname ${host} /home/${username}/zaneyos";
+    fu = "nh os switch --hostname ${host} --update /home/${username}/zaneyos";
+    zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
+    ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+    v = "nvim";
+    cat = "bat";
+    ls = "eza --icons";
+    ll = "eza -lh --icons --grid --group-directories-first";
+    la = "eza -lah --icons --grid --group-directories-first";
+    ".." = "cd ..";
+    zvm = "/home/${username}/.zvm/self/zvm";
+    zb = "zig build";
+
+    ne = "hx /home/${username}/zaneyos/hosts/ashome/";
+
+    gs = "git status -sb";
+    gc = "git commit -am";
+    gp = "git push origin main";
+  };
+in
+{
+  # Home Manager Settings
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
+  home.stateVersion = "24.11";
+  home.sessionPath = [ "/home/${username}/.zvm/bin/" ];
+
+  # Import Program Configurations
+  imports = [
+    ../../config/emoji.nix
+    ../../config/fastfetch
+    ../../config/hyprland.nix
+    ../../config/neovim.nix
+    ../../config/rofi/rofi.nix
+    ../../config/rofi/config-emoji.nix
+    ../../config/rofi/config-long.nix
+    ../../config/swaync.nix
+    ../../config/waybar.nix
+    ../../config/wlogout.nix
+    ../../config/fastfetch
+  ];
+
+  # Place Files Inside Home Directory
+  home.file."Pictures/Wallpapers" = {
+    source = ../../config/wallpapers;
+    recursive = true;
+  };
+  home.file.".config/wlogout/icons" = {
+    source = ../../config/wlogout;
+    recursive = true;
+  };
+  home.file.".face.icon".source = ../../config/face.jpg;
+  home.file.".config/face.jpg".source = ../../config/face.jpg;
+  home.file.".config/swappy/config".text = ''
+    [Default]
+    save_dir=/home/${username}/Pictures/Screenshots
+    save_filename_format=swappy-%Y%m%d-%H%M%S.png
+    show_panel=false
+    line_size=5
+    text_size=20
+    text_font=Ubuntu
+    paint_mode=brush
+    early_exit=true
+    fill_shape=false
+  '';
+
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      # theme = "as_dark";
+
+      editor = {
+        bufferline = "multiple";
+        color-modes = true;
+        line-number = "relative";
+        mouse = false;
+
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        file-picker.hidden = false;
+
+        statusline = {
+          mode.normal = "NORMAL";
+          mode.insert = "INSERT";
+          mode.select = "SELECT";
+          left = ["mode" "spinner" "version-control" "separator" "file-name" "file-modification-indicator" "read-only-indicator"];
+        };
+
+        auto-save.focus-lost = true;
+
+        lsp = {
+          auto-signature-help = false;
+          display-messages = true;
+        };
+
+        indent-guides = {
+          character = "╎";
+          render = true;
+        };
+
+        end-of-line-diagnostics = "hint";
+
+        inline-diagnostics = {
+          cursor-line = "error";
+          other-lines = "disable";
+        };
+      };
+
+      keys = {
+        select = {
+          "{" = "goto_prev_paragraph";
+          "}" = "goto_next_paragraph";
+          "A-x" = "extend_to_line_bounds";
+          "X" = "select_line_above";
+          tab = "extend_parent_node_end";
+          "S-tab" = "extend_parent_node_start";
+        };
+        normal = {
+          "A-x" = "extend_to_line_bounds";
+          "X" = "select_line_above";
+          tab = "move_parent_node_end";
+          "S-tab" = "move_parent_node_start";
+          space = {
+            space = "file_picker";
+            e = ":write";
+            z = ":sh zig build";
+            q = ":write-quit";
+          };
+        };
+        insert = {
+          "S-tab" = "move_parent_node_start";
+        };
+      };
+    };
+
+    languages = {
+      language = [
+        {
+          name = "python";
+          auto-format = true;
+          language-servers = [
+            { name = "pyright"; }
+            { name = "ruff-lsp"; }
+          ];
+          roots = ["uv.lock" ".git" "pyproject.toml"];
+          file-types = ["py" "ipynb"];
+          formatter = {
+            command = "bash";
+            args = ["-c" "uvx ruff check --fix - | uvx ruff format -"];
+          };
+        }
+      ];
+
+      "language-server".pyright = {
+        command = "pyright-langserver";
+        args = ["--stdio"];
+        config = {};
+      };
+
+      "language-server"."ruff-lsp" = {
+        command = "ruff-lsp";
+        args = [];
+        config = {};
+      };
+    };
+  };
+  programs.git = {
+    enable = true;
+    userName = "${gitUsername}";
+    userEmail = "${gitEmail}";
+  };
+
+  programs.zoxide.enable = true;
+  # Create XDG Dirs
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
+
+  dconf.settings = {
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = [ "qemu:///system" ];
+      uris = [ "qemu:///system" ];
+    };
+  };
+
+  # Styling Options
+  stylix.targets.waybar.enable = false;
+  stylix.targets.rofi.enable = false;
+  stylix.targets.hyprland.enable = false;
+  gtk = {
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+  # qt = {
+  #   enable = true;
+  #   style.name = lib.mkDefault "adwaita-dark";
+  #   platformTheme.name = lib.mkDefault "gtk3";
+  # };
+
+
+  # Scripts
+  home.packages = [
+    (import ../../scripts/emopicker9000.nix { inherit pkgs; })
+    (import ../../scripts/task-waybar.nix { inherit pkgs; })
+    (import ../../scripts/squirtle.nix { inherit pkgs; })
+    (import ../../scripts/nvidia-offload.nix { inherit pkgs; })
+    (import ../../scripts/wallsetter.nix {
+      inherit pkgs;
+      inherit username;
+    })
+    (import ../../scripts/web-search.nix { inherit pkgs; })
+    (import ../../scripts/rofi-launcher.nix { inherit pkgs; })
+    (import ../../scripts/screenshootin.nix { inherit pkgs; })
+    (import ../../scripts/list-hypr-bindings.nix {
+      inherit pkgs;
+      inherit host;
+    })
+  ];
+
+  services = {
+    hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          ignore_dbus_inhibit = false;
+          lock_cmd = "hyprlock";
+          };
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
+      };
+    };
+  };
+
+  programs = {
+
+    librewolf.enable = true;
+    gh.enable = true;
+    btop = {
+      enable = true;
+      settings = {
+        vim_keys = true;
+      };
+    };
+    ghostty = {
+      enable = true;
+      settings = {
+        keybind = [
+          # Split Management
+          "alt+super+n=new_split:right"
+          "alt+super+m=new_split:down"
+          "alt+super+j=goto_split:bottom"
+          "alt+super+k=goto_split:top"
+          "alt+super+h=goto_split:left"
+          "alt+super+l=goto_split:right"
+          "alt+super+b=close_surface"
+           
+          # "cmd+shift+up=resize_split:up,20"
+          # "cmd+shift+down=resize_split:down,20"
+          # "cmd+shift+left=resize_split:left,20"
+          # "cmd+shift+right=resize_split:right,20"
+        ];
+    };
+
+    };
+    zed-editor = {
+      enable = true;
+    };
+    yazi = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    kitty = {
+      enable = true;
+      package = pkgs.kitty;
+      themeFile = "GitHub_Dark_High_Contrast";
+      settings = {
+        scrollback_lines = 2000;
+        wheel_scroll_min_lines = 1;
+        window_padding_width = 4;
+        confirm_os_window_close = 0;
+      };
+      extraConfig = ''
+        tab_bar_style fade
+        tab_fade 1
+        active_tab_font_style   bold
+        inactive_tab_font_style bold
+      '';
+      keybindings = {
+        "^[[104;16u" = "neighboring_window left";
+        "ctrl + shift + alt + super + l" = "neighboring_window right";
+        "ctrl + shift + alt + super + j" = "neighboring_window down";
+        "ctrl + shift + alt + super + k" = "neighboring_window up";
+      };
+    };
+     starship = {
+            enable = true;
+            package = pkgs.starship;
+     };
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      profileExtra = ''
+        #if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        #  exec Hyprland
+        #fi
+      '';
+      initExtra = ''
+        fastfetch
+        if [ -f $HOME/.bashrc-personal ]; then
+          source $HOME/.bashrc-personal
+        fi
+      '';
+
+      shellAliases = myAliases;
+    };
+    fish = {
+      enable = true;
+      generateCompletions = true;
+      shellAliases = myAliases;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+        '';
+    };
+    home-manager.enable = true;
+    hyprlock = {
+      enable = true;
+      settings = {
+        general = {
+          disable_loading_bar = true;
+          grace = 10;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+        lib.mkPrio.background = [
+          {
+            path = "/home/${username}/Pictures/Wallpapers/zaney-wallpaper.jpg";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+        image = [
+          {
+            path = "/home/${username}/.config/face.jpg";
+            size = 150;
+            border_size = 4;
+            border_color = "rgb(0C96F9)";
+            rounding = -1; # Negative means circle
+            position = "0, 200";
+            halign = "center";
+            valign = "center";
+          }
+        ];
+        lib.mkPrio.input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            fade_on_empty = false;
+            font_color = "rgb(CFE6F4)";
+            inner_color = "rgb(657DC2)";
+            outer_color = "rgb(0D0E15)";
+            outline_thickness = 5;
+            placeholder_text = "Password...";
+            shadow_passes = 2;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ hosts/asvm/users.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  username,
+  ...
+}:
+
+let
+  inherit (import ./variables.nix) gitUsername;
+in
+{
+  users.users = {
+    "${username}" = {
+      homeMode = "755";
+      isNormalUser = true;
+      description = "${gitUsername}";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "libvirtd"
+        "scanner"
+        "lp"
+        "docker"
+      ];
+      shell = pkgs.fish;
+      ignoreShellProgramCheck = true;
+      packages = with pkgs; [
+        gdb
+        # lldb
+        exercism
+        _1password-cli
+        _1password-gui
+        uv
+        ruff-lsp
+      ];
+    };
+  };
+}
